baf5d898f1fd0500561bf76431d2cf546aa51409
bmhaskar <bmhaskar@gmail.com>
2019-09-22 12:15:46 +0530

    Added a client page

---
+++ client-pages/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import MicrofrontendApp1 from "@bmhaskar/microfrontend1";
+
+function App() {
+  return (
+    <div className="App">
+      <MicrofrontendApp1 />
+    </div>
+  );
+}
+
+export default App;
